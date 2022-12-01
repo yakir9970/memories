509dbd4649a4e1783c2a9ccbfb89dd70bc95ba78
yakir9970 <yakirafeka@gmail.com>
2022-12-01 17:54:58 +0200

    test again

--- server/index.js
+++ server/index.js
@@ -9,12 +9,13 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
-const CONNECTION_URL =
-  "mongodb+srv://yakirtravish:1581997h@cluster0.l8azhwc.mongodb.net/?retryWrites=true&w=majority";
 const PORT = process.env.PORT || 5000;
 
 mongoose
-  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() =>
     app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))
   )
