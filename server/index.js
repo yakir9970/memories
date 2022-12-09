62548f9f6a9b0e4e55e8c7288dee9c8b1f0bb592
yakir9970 <yakirafeka@gmail.com>
2022-12-09 14:49:55 +0200

    get and create controller

--- server/index.js
+++ server/index.js
@@ -1,11 +1,16 @@
 import * as dotenv from "dotenv";
 dotenv.config({ path: "../.env" });
+
 import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
 
+import postRoutes from "./routes/posts.js";
+
 const app = express();
+
+app.use("/posts", postRoutes);
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
