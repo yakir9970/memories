e7d7ed03bc74d4ed852f074737cb916a6a99923d
yakir9970 <yakirafeka@gmail.com>
2022-12-14 13:43:51 +0200

    set up home components

---
+++ client/src/components/Posts/Post/Post.js
@@ -0,0 +1,14 @@
+import React from "react";
+import useStyles from "./styles";
+
+const Post = () => {
+  const classes = useStyles();
+
+  return (
+    <div>
+      <h1>post</h1>
+    </div>
+  );
+};
+
+export default Post;
